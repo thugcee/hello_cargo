5eb9cd6d4c9af5cbe4a77791e1525a63c1fbcee0
Seweryn Niemiec <ser@eioki.eu>
2021-09-30 00:08:10 +0200

    agrs are read into memory and junos is detected

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+extern crate pretty_env_logger;
+extern crate log;
+
 mod args;
 
-use std::fs;
+use std::io::{self, BufRead, BufReader};
+use std::fs::File;
 
 fn main() {
+    pretty_env_logger::init();
     let args = args::parse_args();
     let files = args.values_of(args::FILE).unwrap().collect::<Vec<&str>>();
-    for file in files {
-        println!("Arg file: {}", &file);
+    for filename in files {
+        let reader: Box<dyn io::BufRead> = match filename {
+            "-" => {
+                Box::new(BufReader::new(io::stdin()))
+            },
+            _ => match File::open(&filename) {
+                    Ok(handler) => Box::new(BufReader::new(handler)),
+                    Err(_) => {log::error!("no such file: {}", filename); continue},
+                },
+        };
+        let lines = reader.lines()
+            .map(|l| l.expect("INVALID LINE"))
+            .collect::<Vec<String>>();
+        process(filename, lines);
+    }
+}
+
+fn process(filename: &str, lines: Vec<String>) {
+    log::debug!("{}: {}", filename, lines.len());
+    if lines[2].contains("system {") || lines[2].contains("groups {") {
+        log::info!("detected Junos config");
     }
 }
